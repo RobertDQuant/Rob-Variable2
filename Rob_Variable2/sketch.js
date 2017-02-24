8abca2f5a01c778bb5c3f760911740b7f5ae7015
RobertDQuant <Robertdquant@gmail.com>
2017-02-24 15:05:02 -0500

    Update sketch.js

--- Rob_Variable2/sketch.js
+++ Rob_Variable2/sketch.js
@@ -1,14 +1,14 @@
-var circleX = 50;
+var circleX = 50; //this variable is for where the shape starts
 
 function setup() {
-  createCanvas(600, 480)  
+  createCanvas(600, 480)  //this is the size of the canvas
 }
 
 function draw() {
-  background(250, 0, 250);
-  noStroke();
-  fill(0, 0, 255);
+  background(250, 0, 250); //this is the color of the background
+  noStroke(); // this makes the shape to not have a outline 
+  fill(0, 0, 255); //this is the color of the shape
   ellipse(circleX, 200, 55, 55);
 
-  circleX = circleX + 1;
+  circleX = circleX + 1; // this makes the shape move
 }
